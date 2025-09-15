3210d31c2b9b2ed65d2af48b2c36ed86e8cec3c6
Trm-xnh <lamthanh51124@gmail.com>
2025-09-15 22:09:54 +0700

    thêm 1 tí css và chỉnh lại tí header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,27 @@ import React from "react";
 
 function Header({ cartCount, onToggleCart, showCart }) {
     return (
-        <header className="header">
-            <h1>Mini Web Bán Hàng</h1>
-            <div>
-                <button onClick={onToggleCart}>
-                    {showCart ? "Đóng giỏ" : `Giỏ hàng (${cartCount})`}
-                </button>
-            </div>
-        </header>
+        <>
+            <header className="header">
+                <h1>Mini Web Bán Hàng</h1>
+                <div>
+                    <button onClick={onToggleCart}>
+                        {showCart ? ".//App.jsx" : "Trang chủ"}
+                    </button>
+                </div>
+                <div>
+                    <button onClick={onToggleCart}>
+                        {showCart ? "Đóng giỏ" : "Đăng nhập"}
+                    </button>
+                </div>
+                <div>
+                    <button onClick={onToggleCart}>
+                        {showCart ? "Đóng giỏ" : `Giỏ hàng (${cartCount})`}
+                    </button>
+                </div>
+
+            </header>
+        </>
     );
 }
 
